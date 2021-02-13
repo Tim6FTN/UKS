f7d858da7de066ee89a5822cfc85e5809608c3b7
m-milovanovic <m.milovanovic@outlook.com>
2021-02-13 15:17:58 +0100

    dockerized frontend and database, connected everything with nginx (#34)
    * dockerized frontend and database, and successfully connected everything with nginx
    
    * added travis support for creating frontend image
    
    * travis test
    
    * travis test
    
    * fix travis yaml
    
    * changed docker compose
    
    * removed dockerize-frontend from travis

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,10 +87,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -41,7 +41,7 @@ router.register(r'branch', BranchViewSet)
 router.register(r'commit', CommitViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
 ]
