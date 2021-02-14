06bcca3932a8c6674462910aea9b34d7cab66684
Marko131 <markostanic222@gmail.com>
2021-02-15 00:12:40 +0100

    repo, project, label fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -103,7 +103,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
