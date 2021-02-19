06b7d1f230b7c08ead30d092e359d316952e2ec5
m-milovanovic <m.milovanovic@outlook.com>
2021-02-19 21:25:39 +0100

    NINJA COMMIT TO DEV

--- backend/integration/importer.py
+++ backend/integration/importer.py
@@ -21,7 +21,7 @@ EMPTY_STRING = ''
 SLASH = '/'
 
 API_REPOSITORY_URL = 'https://api.github.com/repos'
-AUTH_HEADER_DICT = {'Authorization': 'token 39fb97c7df39d5124a8836401a8e7e339d5e8855'}
+AUTH_HEADER_DICT = {'Authorization': f"token {os.getenv('GH_ACCESS_TOKEN')}"}
 
 class RepositoryImporter:
 
