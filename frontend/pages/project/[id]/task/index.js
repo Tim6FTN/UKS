ca1b8ef06d8423745d781b9b8779d1c95ede4812
Mihailo Djokic <djokic.mihailo@gmail.com>
2021-02-19 23:39:21 +0100

    task list fix and task index fix

--- frontend/pages/project/[id]/task/index.js
+++ frontend/pages/project/[id]/task/index.js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import NavBar from '../../../../components/util/navbar';
-import Container from '../../../../components/util/container';
-import ProjectWrapper from '../../../../components/project/wrapper';
-import TaskList from '../../../../components/task/list';
-import withAuth from '../../../../components/util/withAuth';
-import TaskService from '../../../../services/taskService';
-import Link from 'next/link';
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ProjectWrapper from "../../../../components/project/wrapper";
+import TaskList from "../../../../components/task/list";
+import TaskService from "../../../../services/taskService";
+import Link from "next/link";
+import { UserContext } from "../../../../contexts/userContext";
+import { ProjectContext } from "../../../../contexts/projectContext";
 
 const Tasks = () => {
   const [tasks, setTasks] = useState([]);
-  const [projectId, setProjectId] = useState(null);
   const router = useRouter();
+  const { user } = useContext(UserContext);
+  const { project } = useContext(ProjectContext);
 
   useEffect(() => {
     if (router.query.id) {
@@ -19,25 +19,30 @@ const Tasks = () => {
     }
   }, [router.query.id]);
 
-  const getTasks = async (paramProjectId) => {
-    const newTasks = (await TaskService.getAll(paramProjectId)).data;
-    setProjectId(paramProjectId);
+  const getTasks = async (projectId) => {
+    const newTasks = (await TaskService.getAll(projectId)).data;
     setTasks(newTasks);
   };
 
   return (
     <>
       <ProjectWrapper>
-        <div className='row'>
-          <h1 className='ml-3'>Tasks</h1>
-          <Link href={`/project/${projectId}/task/new`}>
-            <a className='mx-2 font-weight-bold ml-auto my-auto'>Create task</a>
-          </Link>
+        <div className="row">
+          <h1 className="ml-3">Tasks</h1>
+          {user &&
+            (user?.id === project.owner.id ||
+              project?.collaborators?.some((collab) => collab.id == user.id)) && (
+              <Link href={`/project/${project.id}/task/new`}>
+                <a className="mx-2 font-weight-bold ml-auto my-auto">
+                  Create task
+                </a>
+              </Link>
+            )}
         </div>
-        {tasks && <TaskList tasks={tasks} projectId={projectId} />}
+        {tasks && <TaskList tasks={tasks} projectId={project.id} />}
       </ProjectWrapper>
     </>
   );
 };
 
-export default withAuth(Tasks);
+export default Tasks;
